7205e913d3bddc7ab1a9022602c266c1dc50a50c
Ernesto Lanchares <e.lancha98@gmail.com>
2018-01-05 18:12:40 +0100

    molaria que se pudieran referenciar a personas (como @Miquel ) en los commits

--- upload_file.sh
+++ upload_file.sh
@@ -17,6 +17,6 @@ upload(){
   python -c "import sys, json; print(json.load(sys.stdin)['sha'])")
   { echo -n "{\"path\":\"$DEST\", \"message\":\"$MSG\", \"sha\":\"$SHA\", \"content\":\""; base64 -w 0 $NAME.pdf; echo -n "\"}"; } | curl -s -H "Content-Type: application/json" -u $user:$password -X PUT -d @- \
   "https://api.github.com/repos/ApuntsFME/apuntsfme.github.io/contents$DEST" > /dev/null
-  #{ echo "{\"path\":\"$DEST\", \"message\":\"$MSG\", \"sha\":\"$SHA\", \"content\":\""; base64 $NAME.pdf; echo "\"}"; } | tr "\n" " "
+  #{echo "{\"path\":\"$DEST\", \"message\":\"$MSG\", \"sha\":\"$SHA\", \"content\":\""; base64 $NAME.pdf; echo "\"}"; } | tr "\n" " "
   echo "Molt bé! Ja s'ha actualitzat, a no ser que et digui algo raro o que hagi passat algo i no t'ho hagi dit, mira Telegram per assegurar-te'n."
 }
