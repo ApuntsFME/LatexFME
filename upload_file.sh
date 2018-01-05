a1bade0e66814df9da84b05ee2feeab2856e2387
Ernesto Lanchares <e.lancha98@gmail.com>
2018-01-05 18:08:06 +0100

    puto Miquel aprende a hacer scripts

--- upload_file.sh
+++ upload_file.sh
@@ -14,7 +14,7 @@ upload(){
   echo ""
   echo "Això trigarà una mica... Keep calm and carry on!"
   SHA=$(curl -s -u $user:$password "https://api.github.com/repos/ApuntsFME/apuntsfme.github.io/contents$DEST" | \
-  python -c "import sys, json; print json.load(sys.stdin)['sha']")
+  python -c "import sys, json; print(json.load(sys.stdin)['sha'])")
   { echo -n "{\"path\":\"$DEST\", \"message\":\"$MSG\", \"sha\":\"$SHA\", \"content\":\""; base64 -w 0 $NAME.pdf; echo -n "\"}"; } | curl -s -H "Content-Type: application/json" -u $user:$password -X PUT -d @- \
   "https://api.github.com/repos/ApuntsFME/apuntsfme.github.io/contents$DEST" > /dev/null
   #{ echo "{\"path\":\"$DEST\", \"message\":\"$MSG\", \"sha\":\"$SHA\", \"content\":\""; base64 $NAME.pdf; echo "\"}"; } | tr "\n" " "
