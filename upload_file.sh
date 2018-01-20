b0ff1cd8ed6fe36dd420c87cf978dbde740c4211
oscarbenedito <oscar.benedito2@gmail.com>
2018-01-20 21:05:21 +0100

    Makefiles i scripts retocats

--- upload_file.sh
+++ upload_file.sh
@@ -18,8 +18,10 @@ upload(){
   { echo -n "{\"path\":\"$DEST\", \"message\":\"$MSG\", \"sha\":\"$SHA\", \"content\":\""; base64 -w 0 $NAME.pdf; echo -n "\"}"; } | curl -s -H "Content-Type: application/json" -u $user:$password -X PUT -d @- \
   "https://api.github.com/repos/ApuntsFME/apuntsfme.github.io/contents$DEST" > /dev/null
   #{echo "{\"path\":\"$DEST\", \"message\":\"$MSG\", \"sha\":\"$SHA\", \"content\":\""; base64 $NAME.pdf; echo "\"}"; } | tr "\n" " "
-  echo "Molt bé! Ja s'ha actualitzat, a no ser que et digui algo raro o que hagi passat algo i no t'ho hagi dit, mira Telegram per assegurar-te'n."
+  echo ""
+  echo "Ja s'ha actualitzat, mira Telegram per assegurar-te'n."
   else 
+    echo ""
     echo "Eps! No compila"
   fi
 }
