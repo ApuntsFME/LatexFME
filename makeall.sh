86961a9a46db25994eface6954539de7f67ee9d9
System User <e.lancha98@gmail.com>
2018-10-18 11:14:03 +0200

    Colorines :yay:

--- makeall.sh
+++ makeall.sh
@@ -1,66 +1,69 @@
 #!/bin/bash
 
+COLOR='\033[0;32m'
+NC='\033[0m' #no color
+
 if test "$1" = 'web'; then
 	read -p "Username: " usr
 	read -s -p "Password: " psw
 	
 	cd Algebra_Lineal
-	echo Algebra_Lineal
+	echo -e ${COLOR}Algebra_Lineal${NC}
 	#(printf %s\\n%s\\n "$usr" "$psw"; cat) | ./upload_pdf
 	cd ../Algebra_Lineal_Numerica
-	echo Algebra_Lineal_Numerica
+	echo -e ${COLOR}Algebra_Lineal_Numerica${NC}
 	(printf %s\\n%s\\n "$usr" "$psw"; cat) | ./upload_pdf
 	cd ../Algebra_Multilineal_i_Geometria_Projectiva
-	echo Algebra_Multilineal_i_Geometria_Projectiva
+	echo -e ${COLOR}Algebra_Multilineal_i_Geometria_Projectiva${NC}
 	(printf %s\\n%s\\n "$usr" "$psw"; cat) | ./upload_pdf
 	cd ../Calcul_Integral
-	echo Calcul_Integral
+	echo -e ${COLOR}Calcul_Integral${NC}
 	(printf %s\\n%s\\n "$usr" "$psw"; cat) | ./upload_pdf
 	cd ../Calcul_Numeric
-	echo Calcul_Numeric
+	echo -e ${COLOR}Calcul_Numeric${NC}
 	(printf %s\\n%s\\n "$usr" "$psw"; cat) | ./upload_pdf
 	cd ../Estructures_Algebraiques
-	echo Estructures_Algebraiques
+	echo -e ${COLOR}Estructures_Algebraiques${NC}
 	(printf %s\\n%s\\n "$usr" "$psw"; cat) | ./upload_pdf
 	cd ../Fisica
-	echo Fisica
+	echo -e ${COLOR}Fisica${NC}
 	(printf %s\\n%s\\n "$usr" "$psw"; cat) | ./upload_pdf
 	cd ../Programacio_Matematica
-	echo Programacio_Matematica
+	echo -e ${COLOR}Programacio_Matematica${NC}
 	(printf %s\\n%s\\n "$usr" "$psw"; cat) | ./upload_pdf
 	cd ../Teoria_de_la_Probabilitat
-	echo Teoria_de_la_Probabilitat
+	echo -e ${COLOR}Teoria_de_la_Probabilitat${NC}
 	(printf %s\\n%s\\n "$usr" "$psw"; cat) | ./upload_pdf
 	cd ../Topologia
-	echo Topologia
+	echo -e ${COLOR}Topologia${NC}
 else
 	cd Algebra_Lineal
-	echo Algebra_Lineal
+	echo -e ${COLOR}Algebra_Lineal${NC}
 	make silentall
 	cd ../Algebra_Lineal_Numerica
-	echo Algebra_Lineal_Numerica
+	echo -e ${COLOR}Algebra_Lineal_Numerica${NC}
 	make silentall
 	cd ../Algebra_Multilineal_i_Geometria_Projectiva
-	echo Algebra_Multilineal_i_Geometria_Projectiva
+	echo -e ${COLOR}Algebra_Multilineal_i_Geometria_Projectiva${NC}
 	make silentall
 	cd ../Calcul_Integral
-	echo Calcul_Integral
+	echo -e ${COLOR}Calcul_Integral${NC}
 	make silentall
 	cd ../Calcul_Numeric
-	echo Calcul_Numeric
+	echo -e ${COLOR}Calcul_Numeric${NC}
 	make silentall
 	cd ../Estructures_Algebraiques
-	echo Estructures_Algebraiques
+	echo -e ${COLOR}Estructures_Algebraiques${NC}
 	make silentall
 	cd ../Fisica
-	echo Fisica
+	echo -e ${COLOR}Fisica${NC}
 	make silentall
 	cd ../Programacio_Matematica
-	echo Programacio_Matematica
+	echo -e ${COLOR}Programacio_Matematica${NC}
 	make silentall
 	cd ../Teoria_de_la_Probabilitat
-	echo Teoria_de_la_Probabilitat
+	echo -e ${COLOR}Teoria_de_la_Probabilitat${NC}
 	make silentall
 	cd ../Topologia
-	echo Topologia
+	echo -e ${COLOR}Topologia${NC}
 fi
